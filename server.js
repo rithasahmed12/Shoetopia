1de6b386aa0d3d6d40954b2bdc514c268660563b
rithasahmed12 <145911740+rithasahmed12@users.noreply.github.com>
2024-01-08 12:29:06 +0530

    static directory changed

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:true}));
 app.set('view engine','ejs');
 
 
-app.use(express.static(path.join(__dirname,'public','assets')));
+// app.use(express.static(path.join(__dirname,'public','assets')));
 app.use(express.static(path.join(__dirname,'public')));
 
 
